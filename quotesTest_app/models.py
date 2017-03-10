3a24d08e9e096380a126afd8e35e45a033e69e6c
Brendan Greene <brendanjgreene@gmail.com>
2017-03-10 18:22:11 +0000

    template to display Quotes Challenge

--- quotesTest_app/models.py
+++ quotesTest_app/models.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+
 class Quote(models.Model):
 
     class Meta:
--- quotesTest_app/views.py
+++ quotesTest_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from quotesTest_app.models import Quote
 
 # Create your views here.
+def get_quotes(request):
+    return render(request, "quotesTest/home.html",
+                  {'quote_list': Quote.objects.all()})
